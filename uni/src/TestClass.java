e2e6187b2d6abebf6e73e8c6ac065145a358c244
Marco Villotta <gmarcovil@gmail.com>
2020-12-21 23:40:11 +0100

    add abstractiontypes package add TestClass

---
+++ uni/src/TestClass.java
@@ -0,0 +1,10 @@
+public class TestClass {
+
+    public static void main (String[] args) {
+        int[] arr1 = { 45, 32, 24, 1};
+        abstractiontypes.MyInsertionSort.printArray(arr1);
+        abstractiontypes.MyInsertionSort.insertionSort(arr1);
+        abstractiontypes.MyInsertionSort.printArray(arr1);
+    }
+
+}
---
+++ uni/src/abstractiontypes/MyInsertionSort.java
@@ -0,0 +1,38 @@
+package abstractiontypes;
+
+public class MyInsertionSort {
+    static int[] arr1 = {10, 34, 2, 56, 7, 67, 88, 42};
+
+    public static void main(String a[]) {
+        printArray(arr1);
+        insertionSort(arr1);
+        printArray(arr1);
+    }
+
+    // REQUIRES: arr to be non null
+    // MODIFIES: arr
+    // EFFECTS: orders the array arr
+    public static void insertionSort(int[] arr) {
+        int temp;
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            }
+        }
+    }
+
+    // REQUIRES: arr to be non null
+    // EFFECTS: prints the elements of arr to Standard Output
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i);
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+
+}
